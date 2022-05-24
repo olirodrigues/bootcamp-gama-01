bde79ef234b26871a674a298d592414eab0b67be
olirodrigues <olivia.rt@gmail.com>
2022-05-23 23:48:30 -0300

    Add: component menu

---
+++ Web/src/components/Menu/Menu.style.tsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const ContainerMenu = styled.div`
+  background-color: #eeeeee;
+  height: 100vh;
+  width: 268px;
+  padding-inline: 15px;
+
+  img {
+    margin-block: 36px 68px;
+  }
+`;
+
+export const ContaiverNav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
